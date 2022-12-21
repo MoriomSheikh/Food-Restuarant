93889630b75ffa02cb213cb6f3a1dcbe3ae11c51
MoriomSheikh <sheikhmoriom24@gmail.com>
2022-12-22 03:41:21 +0600

    add a slider

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@ menu.onclick = () => {
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 }
+
+Window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  console.log('Button click');
+}
+
+document.querySelector('#search-icon').onclick = () => {
+  document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () => {
+  document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
